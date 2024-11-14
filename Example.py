cd581cb77b6ceef7c0dca4f60e22e97b79192fbc
Better_OIer <2155173747@qq.com>
2024-11-14 22:55:28 +0800

    202411142255

---
+++ Example.py
@@ -0,0 +1,21 @@
+from CovertPDFtoPNG import covertPDFtoImg
+from MergePNG import mergeImgs
+
+name = "Your_PDF_Name"
+imgFormat = "jpg"
+
+inFile = f"Your_File_Path\\{name}.pdf"
+tempDir = f"Your_File_Path\\{name}temp"
+outDir = f"Your_File_Path\\{name}"
+
+confirm = input(f"[Inform] Run Program 1?")
+if confirm == "":
+    covertPDFtoImg(inFile, tempDir, name, imgFormat, 2)
+else:
+    print(f"[Error] Program 1 terminated by user!")
+
+confirm = input(f"[Inform] Run Program 2?")
+if confirm == "":
+    mergeImgs(tempDir, outDir, imgFormat, 5, 8, True, 1)
+else:
+    print(f"[Error] Program 2 terminated by user!")
